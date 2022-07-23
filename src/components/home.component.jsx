b2c729862e094b8b60bcd0bbe43414e9cedcb144
vinodkotagiri <vinodkotagiri@icloud.com>
2022-07-23 10:23:44 +0530

    fixed navbar issue

--- src/components/home.component.jsx
+++ src/components/home.component.jsx
@@ -4,8 +4,8 @@ import { AiOutlineRight, AiOutlineArrowRight } from 'react-icons/ai';
 const Home = () => {
   return (
     <div className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
-      <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row '>
-        <div className='flex flex-col justify-center h-full'>
+      <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
+        <div className='flex flex-col justify-center h-full '>
           <h2 className='text-4xl sm:text-7xl font-bold text-white'>
             I'm a Full Stack Developer
           </h2>
@@ -35,7 +35,7 @@ const Home = () => {
           </div>
         </div>
 
-        <div>
+        <div className='mt-5'>
           <img
             src={HeroImage}
             alt='hero-profile'
--- src/components/navbar.component.jsx
+++ src/components/navbar.component.jsx
@@ -1,59 +1,57 @@
-import React, { Fragment, useState } from 'react';
+import React, { useState } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
 const Navbar = () => {
   const [navToggled, setNavToggled] = useState(false);
   return (
-    <Fragment>
-      <div className='flex justify-between items-center w-full h-20 text-white bg-black fixed px-4'>
-        <div>
-          <h1 className='p-1 text-5xl ml-2 font-signature text-white'>
-            Vinod Kotagiri
-          </h1>
-        </div>
-        <ul className='hidden md:flex '>
-          <li className='px-4 uppercase cursor-pointer hover:scale-110 duration-300 hover:text-orange-500'>
+    <div className='flex justify-between items-center w-full h-20 text-white bg-black fixed px-4 mb-6'>
+      <div>
+        <h1 className='p-1 text-4xl ml-2 font-signature text-white'>
+          Vinod Kotagiri
+        </h1>
+      </div>
+      <ul className='hidden md:flex '>
+        <li className='px-4 uppercase cursor-pointer hover:scale-110 duration-300 hover:text-orange-500'>
+          home
+        </li>
+        <li className='px-4 uppercase cursor-pointer hover:scale-110 duration-300 hover:text-orange-500 '>
+          about
+        </li>
+        <li className='px-4 uppercase cursor-pointer hover:scale-110 duration-300 hover:text-orange-500 '>
+          portfoilo
+        </li>
+        <li className='px-4 uppercase cursor-pointer hover:scale-110 duration-300 hover:text-orange-500 '>
+          skills
+        </li>
+        <li className='px-4 uppercase cursor-pointer hover:scale-110 duration-300 hover:text-orange-500 '>
+          contact
+        </li>
+      </ul>
+      <div
+        onClick={() => setNavToggled(!navToggled)}
+        className='cursor-pointer pr-4 md:hidden z-10'
+      >
+        {navToggled ? <FaTimes size={36} /> : <FaBars size={36} />}
+      </div>
+      {navToggled && (
+        <ul className='flex flex-col justify-center items-center absolute top-0 left-0 h-screen w-full bg-gradient-to-b from-black to-gray-600'>
+          <li className='px-4 uppercase cursor-pointer hover:scale-110 duration-300 text-gray-500 py-6 text-4xl'>
             home
           </li>
-          <li className='px-4 uppercase cursor-pointer hover:scale-110 duration-300 hover:text-orange-500 '>
+          <li className='px-4 uppercase cursor-pointer hover:scale-110 duration-300 text-gray-500 py-6 text-4xl'>
             about
           </li>
-          <li className='px-4 uppercase cursor-pointer hover:scale-110 duration-300 hover:text-orange-500 '>
+          <li className='px-4 uppercase cursor-pointer hover:scale-110 duration-300 text-gray-500 py-6 text-4xl'>
             portfoilo
           </li>
-          <li className='px-4 uppercase cursor-pointer hover:scale-110 duration-300 hover:text-orange-500 '>
+          <li className='px-4 uppercase cursor-pointer hover:scale-110 duration-300 text-gray-500 py-6 text-4xl'>
             skills
           </li>
-          <li className='px-4 uppercase cursor-pointer hover:scale-110 duration-300 hover:text-orange-500 '>
+          <li className='px-4 uppercase cursor-pointer hover:scale-110 duration-300 text-gray-500 py-6 text-4xl'>
             contact
           </li>
         </ul>
-        <div
-          onClick={() => setNavToggled(!navToggled)}
-          className='cursor-pointer pr-4 md:hidden z-10'
-        >
-          {navToggled ? <FaTimes size={48} /> : <FaBars size={48} />}
-        </div>
-        {navToggled && (
-          <ul className='flex flex-col justify-center items-center absolute top-0 left-0 h-screen w-full bg-gradient-to-b from-black to-gray-600'>
-            <li className='px-4 uppercase cursor-pointer hover:scale-110 duration-300 text-gray-500 py-6 text-4xl'>
-              home
-            </li>
-            <li className='px-4 uppercase cursor-pointer hover:scale-110 duration-300 text-gray-500 py-6 text-4xl'>
-              about
-            </li>
-            <li className='px-4 uppercase cursor-pointer hover:scale-110 duration-300 text-gray-500 py-6 text-4xl'>
-              portfoilo
-            </li>
-            <li className='px-4 uppercase cursor-pointer hover:scale-110 duration-300 text-gray-500 py-6 text-4xl'>
-              skills
-            </li>
-            <li className='px-4 uppercase cursor-pointer hover:scale-110 duration-300 text-gray-500 py-6 text-4xl'>
-              contact
-            </li>
-          </ul>
-        )}
-      </div>
-    </Fragment>
+      )}
+    </div>
   );
 };
 
